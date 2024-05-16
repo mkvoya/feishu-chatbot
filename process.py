9b7137ba0420d671fa35eeab1b64629af57a58b8
Mingkai Dong <mingkaidong@gmail.com>
2024-05-16 04:51:12 -0700

    update main logic to make it work

--- process.py
+++ process.py
@@ -101,13 +101,8 @@ users_info = {}
 token_manager = TokenManager(app_id=app_id, app_secret=app_secret)
 sender = LarkMsgSender(token_manager)
 
-
-class UserInfo():
-    llmapi_key = None
-    llmapi_host = 'https://api.llmapi.io'
-    client = None
-    bot = 'chatgpt'
-
+processed_message_ids = set()
+pending_messages = []
 
 async def reply_message(input: dict):
     reply = ""
@@ -127,76 +122,29 @@ async def reply_message(input: dict):
         await sender.send(reply, input["event"]["message"]["message_id"])
         return
 
-    user_id = input['event']['sender']['sender_id']['user_id']
-    if user_id not in users_info:
-        users_info[user_id] = UserInfo()
-
     prompt = content['text']
+    pending_messages.append(prompt)
 
     help_msg = '\n'
     help_msg += '发送 "!help" 给机器人，机器人会显示帮助信息\n'
     help_msg += '发送 "!reset" 给机器人，机器人会重置对话状态\n'
-    help_msg += '发送 "!llmapi_host http://your.llmapi.host.com" 给机器人，设置llmapi地址，默认为https://api.llmapi.io\n'
-    help_msg += '发送 "!llmapi_key xxx" 给机器人，设置llmapi key，如果是自建llmapi，则无需设置\n'
     help_msg += '发送 "!bot xxx" 给机器人，指定对应机器人类型，比如chatgpt、welm、newbing...\n'
     help_msg += '发送 "!show" 给机器人，展示当前状态\n'
     help_msg += '\n'
-    help_msg += '用法：\n'
-    help_msg += '如果你是自建llmapi，那么你需要先发送 "!llmapi_host http://your.llmapi.host.com" 给机器人，设置llmapi地址\n'
-    help_msg += '如果你是使用官方llmapi，那么你需要先发送 "!llmapi_key xxx" 给机器人，设置llmapi key，apikey在https://llmapi.io获取\n'
-    help_msg += '默认是chatgpt，如需更改机器人，通过!bot指定，比如 "!bot welm"，目前支持的机器人有：chatgpt、welm、newbing\n'
-    help_msg += '然后发送任意消息给机器人，机器人会自动回复\n'
-    help_msg += '如果你想重置对话状态，可以发送 "!reset" 给机器人\n'
-    help_msg += '如果你想查看当前状态，可以发送 "!show" 给机器人，遇到问题时这个命令很有帮助哦\n'
 
-    def get_key(key):
-        if key is None:
-            return 'None'
-        else:
-            return key[0:3] + '*' * (len(key) - 6) + key[-3:]
     if prompt == '!help':
         reply = help_msg
     elif prompt == '!show':
         reply = '当前状态：\n'
-        reply += 'llmapi地址：' + users_info[user_id].llmapi_host + '\n'
-        reply += 'llmapi key：' + get_key(users_info[user_id].llmapi_key) + '\n'
-        reply += '机器人类型：' + users_info[user_id].bot + '\n'
-        reply += '对话状态：' + \
-            ('已初始化' if users_info[user_id].client is not None else '未初始化') + '\n'
-    elif prompt == '!reset':
-        if users_info[user_id].client is not None:
-            await users_info[user_id].client.end_session()
-        users_info[user_id].client = None
-        reply = '重置对话状态\n'
-    elif prompt.startswith('!llmapi_host'):
-        if len(prompt.split(' ')) != 2:
-            reply = '参数错误，正确用法：!llmapi_host http://your.llmapi.host.com\n'
-        elif not prompt.split(' ')[1].startswith('http'):
-            reply = '参数错误，正确用法：!llmapi_host http://your.llmapi.host.com\n'
-        else:
-            users_info[user_id].llmapi_host = prompt.split(' ')[1]
-            reply = '设置llmapi地址为' + users_info[user_id].llmapi_host + '\n'
-    elif prompt.startswith('!llmapi_key'):
-        if len(prompt.split(' ')) != 2:
-            reply = '参数错误，正确用法：!llmapi_key xxx\n'
-        else:
-            users_info[user_id].llmapi_key = prompt.split(' ')[1]
-            reply = '设置llmapi key为' + \
-                get_key(users_info[user_id].llmapi_key) + '\n'
-            reply += '建议把你刚刚发的消息删除，防止泄露llmapi key\n'
     elif prompt.startswith('!bot'):
-        users_info[user_id].bot = prompt.split(' ')[1]
-        reply = '设置机器人为' + users_info[user_id].bot + '\n'
+        reply = '设置机器人为'
     else:
-        reply = "default"
+        reply = "sync to wx:\n" + content['text']
     await sender.send(reply, input["event"]["message"]["message_id"])
 
 
 class LarkMsgType(BaseModel):
     encrypt: str
-
-
-processed_message_ids = set()
 
 
 @app.post("/")
@@ -213,3 +161,12 @@ async def process(message: LarkMsgType, request: Request, background_tasks: Back
             reply_message, plaintext)  # reply in background
 
     return {'message': 'ok'}  # 接受到消息后，立即返回ok，避免客户端重试
+
+@app.get("/fetch/sdfkj13lkj")
+async def fetch(request: Request):
+    global pending_messages
+    if len(pending_messages):
+        msg = pending_messages[0]
+        pending_messages = pending_messages[1:]
+        return {'ok': 'ok', 'message': msg}
+    return {'ok':'no', 'message': ''}  # 接受到消息后，立即返回ok，避免客户端重试
